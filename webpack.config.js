74bce3c13a773ca12c61866ad9960e10c4c324b1
Awais Hussain <awais@arachnys.com>
2017-10-09 21:14:52 +0100

    Initial commit of D3 frontend

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+      path: __dirname + "/dist",
+      filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    },
+    externals: {
+      "jquery": "jQuery"
+    }
+  }
